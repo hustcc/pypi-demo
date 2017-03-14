0b889846bf18558050a4b662cb15fc0624aaec10
hustcc <i@atool.org>
2017-03-14 10:54:41 +0800

    pypidemo

---
+++ README.md
@@ -0,0 +1,15 @@
+# pypidemo
+
+> Just a simple pypi package code structure. You can use it to **register pypi package** name.
+
+
+## How to use
+
+1. clone the code into your folder.
+2. modify `setup.py`, and update the package name(**pypidemo**) to yours.
+3. `python setup.py sdist upload` to register the package name.
+
+
+## LICENSE
+
+MIT @[hustcc](https://github.com/hustcc).
---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import setuptools  # noqa
+from distutils.core import setup
+import io
+import re
+import os
+
+
+DOC = 'Document on <https://github.com/hustcc/pypi-demo>'
+
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name='pypidemo',
+      version=find_version('pypidemo/__init__.py'),
+      description=('pypidemo'),
+      long_description=DOC,
+      author='hustcc',
+      author_email='i@hust.cc',
+      url='https://github.com/hustcc/hust',
+      license='MIT',
+      install_requires=[
+      ],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities'
+      ],
+      keywords='pypidemo, hustcc, github',
+      include_package_data=True,
+      zip_safe=False,
+      packages=['pypidemo'],
+      entry_points={
+        'console_scripts': ['pypidemo=pypidemo.cli:run']
+      })
